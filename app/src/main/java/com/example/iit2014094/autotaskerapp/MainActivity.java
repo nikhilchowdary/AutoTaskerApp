eb566615c69cb44ef0d4566bbb10ea019eead3db
nikhilchowdary <iit2014094@iiita.ac.in>
2017-03-24 12:31:50 +0530

    Added Layouts

--- app/src/main/java/com/example/iit2014094/autotaskerapp/MainActivity.java
+++ app/src/main/java/com/example/iit2014094/autotaskerapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -81,7 +82,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
