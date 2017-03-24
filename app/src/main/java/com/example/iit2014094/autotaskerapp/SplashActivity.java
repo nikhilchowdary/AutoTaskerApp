7ce6960ff617a365949c28f8a3fc1ab4e0a90e50
nikhilchowdary <iit2014094@iiita.ac.in>
2017-03-24 14:57:36 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/iit2014094/autotaskerapp/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.iit2014094.autotaskerapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by iit2014094 on 3/24/2017.
+ */
+
+public class SplashActivity extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
